92296476601e129078eb28cf6359264667e6b484
Aishwarya1907 <54067486+Aishwarya1907@users.noreply.github.com>
2019-08-19 16:46:03 -0700

    Create multiplefive.cpp

---
+++ multiplefive.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t=0,n;
+    cin>>n;
+    for(int i=1;i<=5;i++)
+    {
+        t=i*n;
+        cout<<t<<" ";
+    }
+    return 0;
+}
