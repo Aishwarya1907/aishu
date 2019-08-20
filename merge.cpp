e06d68b15e38bbbae6ec2208c8e849e88d9abe23
Aishwarya1907 <54067486+Aishwarya1907@users.noreply.github.com>
2019-08-19 17:11:06 -0700

    Create merge.cpp

---
+++ merge.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[1000];
+    int i,j,temp,n;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(a[j] < a[i])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
